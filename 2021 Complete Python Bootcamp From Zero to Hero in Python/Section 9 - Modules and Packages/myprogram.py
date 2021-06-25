3f86081a0a8fe0c500c505553921e1adec232783
Akshay S Kadakol <akshayskadakol@gmail.com>
2021-06-25 21:31:35 +0530

    Section 9

---
+++ 2021 Complete Python Bootcamp From Zero to Hero in Python/Section 9 - Modules and Packages/myprogram.py
@@ -0,0 +1,8 @@
+from mymodule import my_func
+from MyMainPackage import mainscript
+from MyMainPackage.SubPackage import mysubscript
+
+my_func()
+
+mainscript.main_report()
+mysubscript.sub_report()
---
+++ 2021 Complete Python Bootcamp From Zero to Hero in Python/Section 9 - Modules and Packages/one.py
@@ -0,0 +1,14 @@
+def func():
+	print('func in one.py')
+
+def function():
+	pass
+
+def function1():
+	pass
+
+if __name__ == '__main__':
+	print('one.py being run directly')
+	function()
+	function1()
+	func()
